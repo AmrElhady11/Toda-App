5ae5f1b95d1fdcf827abeb19fbc3114b081c0b3b
AmrElhady11 <amr.elhady.programs@gmail.com>
2025-07-06 20:11:32 +0300

    adding RouterLocator as a bean to define routes

--- gateway/src/main/java/com/toda_app/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/toda_app/gateway/GatewayApplication.java
@@ -2,12 +2,25 @@ package com.toda_app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("Todo-service", r -> r.path("/todos/**","/api/v1/tasks/**").uri("http://localhost:7070"))
+				.route("authentication-service", r -> r.path("/api/v1/auth/**","/api/v1/account/**","/auth/**").uri("http://localhost:8080"))
+				.build();
+	}
+
 
 }
